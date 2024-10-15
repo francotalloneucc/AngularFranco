a6bd7ba1ae70a99c7bfb1a4da7399a1c893619c8
franco tallone <francotallone01@hotmail.com>
2024-10-15 19:16:05 +0000

    Updated Program.cs

--- EmployeeCrudApi/EmployeeCrudApi/Program.cs
+++ EmployeeCrudApi/EmployeeCrudApi/Program.cs
@@ -1,7 +1,6 @@
 using EmployeeCrudApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +12,17 @@ builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
            .AllowAnyHeader();
 }));
 
+// Obtener la cadena de conexión de una variable de entorno o de la configuración
+var connectionString = Environment.GetEnvironmentVariable("cnn_string_qa")
+                      ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("La cadena de conexión no está configurada.");
+}
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-   options.UseSqlServer(
-       builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
