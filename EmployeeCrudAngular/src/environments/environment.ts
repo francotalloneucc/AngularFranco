e6c65be4ef6b738917c1c68bb5783388e1063bef
franco tallone <francotallone01@hotmail.com>
2024-10-19 18:00:19 +0000

    Updated environment.ts

--- EmployeeCrudAngular/src/environments/environment.ts
+++ EmployeeCrudAngular/src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:7150/api/Employee'  // URL de la API para desarrollo
+  apiUrl: (typeof window !== 'undefined' && window.env && window.env.apiUrl) 
+             ? window.env.apiUrl
+             : 'http://localhost:7150/api/Employee' // Valor por defecto
 };
 
 
