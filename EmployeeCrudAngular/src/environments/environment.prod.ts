382480c0fdf2aa0573bb95603881f2572e87b904
franco tallone <francotallone01@hotmail.com>
2024-10-19 19:01:31 +0000

    Updated environment.prod.ts

--- EmployeeCrudAngular/src/environments/environment.prod.ts
+++ EmployeeCrudAngular/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: (typeof window !== 'undefined' && window.env && window.env.apiUrl) 
              ? window.env.apiUrl
-             : 'http://localhost:7150/api/Employee' // Valor por defecto
+             : 'ftm-crud-api-qa.brazilsouth.azurecontainer.io/api/Employee' // Valor por defecto
 };
